cadf62f230efa71ef3da776f6afc1120b9382b8e
siddarth1128 <imsiddarth28@gmail.com>
2025-04-14 17:20:36 +0530

    Add Day11.cpp

---
+++ Day11.cpp
@@ -0,0 +1,39 @@
+// C++ program to find Maximum Product Subarray using 
+// Traversal From Starting and End of an Array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// function to find the product of max product subarray.
+int maxProduct(vector<int> &arr) {
+  	int n = arr.size();
+    int maxProd = INT_MIN;
+  
+    // leftToRight to store product from left to Right
+    int leftToRight = 1;
+  
+    // rightToLeft to store product from right to left
+    int rightToLeft = 1;
+  
+    for (int i = 0; i < n; i++) {
+        if (leftToRight == 0)
+            leftToRight = 1;
+        if (rightToLeft == 0)
+            rightToLeft = 1;
+      
+        // calculate product from index left to right
+        leftToRight *= arr[i];
+      
+        // calculate product from index right to left
+        int j = n - i - 1;
+        rightToLeft *= arr[j];
+        maxProd = max({leftToRight, rightToLeft, maxProd});
+    }
+    return maxProd;
+}
+
+int main() {
+    vector<int> arr = { -2, 6, -3, -10, 0, 2 };
+    cout << maxProduct(arr);
+    return 0;
+}
