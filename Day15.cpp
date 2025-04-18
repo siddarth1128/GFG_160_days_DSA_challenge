d4f3662730b8553b4e31638fac604ee4c80d8c5a
siddarth1128 <imsiddarth28@gmail.com>
2025-04-18 20:11:13 +0530

    Add Day15.cpp

--- Day15.cpp
+++ Day15.cpp
@@ -1,48 +1,48 @@
-#include <iostream>
-using namespace std;
-
-string trimLeadingZeros(string &s){
-    string ss=" ";
-    for(int i=0;i<s.size();i++){
-        if(s[i]=='1'){
-            return s.substr(i,s.size());
-            break;
-        }
-    }
-}
-string addBinary(string &s1, string &s2) {
-    s1 = trimLeadingZeros(s1);
-    s2 = trimLeadingZeros(s2);
+class Solution {
+    public:
+      string startzero(string &s){
+          for(int i = 0; i < s.size(); i++){
+              if(s[i] == '1'){
+                  return s.substr(i);
+              }
+          }
+          return "0"; // or return ""; depending on your logic
+      }
   
-    int n = s1.size();  
-    int m = s2.size(); 
-    
-    if (n < m) {
-        return addBinary(s2, s1);
-    }
-    
-    int j = m - 1;  
-    int carry = 0; 
-    for (int i = n - 1; i >= 0; i--) {
-        int bit1 = s1[i] - '0';  
-        int sum = bit1 + carry;
-        if (j >= 0) {
-            int bit2 = s2[j] - '0';  
-            sum += bit2;
-            j--;
-        }
-        int bit = sum % 2;
-        carry = sum / 2;
-        s1[i] = (char)(bit + '0');
-    }
-    if (carry > 0) {
-        s1 = '1' + s1;
-    }
-    return s1;
-}
-int main() {
+      string addBinary(string& s1, string& s2) {
+          string str1 = startzero(s1);
+          string str2 = startzero(s2);
   
-    string s1 = "1101", s2 = "111";
-    cout << addBinary(s1, s2) << endl;
-    return 0;
-}
+          int n = str1.size();
+          int m = str2.size();
+  
+          if(n < m){
+              return addBinary(s2, s1); // swap and recurse
+          }
+  
+          int carry = 0;
+          int j = m - 1;
+  
+          for(int i = n - 1; i >= 0; i--){
+              int bit1 = str1[i] - '0';
+              int sum = bit1 + carry;
+  
+              if(j >= 0){
+                  int bit2 = str2[j] - '0';
+                  sum += bit2;
+                  j--;
+              }
+  
+              int bit = sum % 2;
+              carry = sum / 2;
+              str1[i] = (char)(bit + '0');
+          }
+  
+          if(carry > 0){
+              str1 = '1' + str1;
+          }
+  
+          return str1;
+      }
+  };
+  
